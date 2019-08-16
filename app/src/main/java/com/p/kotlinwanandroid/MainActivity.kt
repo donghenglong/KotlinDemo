3836ec5efd20365634b1945041394a680ccbc60b
donghenglong <1013896293@qq.com>
2019-08-16 17:10:19 +0800

    测试

--- app/src/main/java/com/p/kotlinwanandroid/MainActivity.kt
+++ app/src/main/java/com/p/kotlinwanandroid/MainActivity.kt
@@ -2,11 +2,20 @@ package com.p.kotlinwanandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var n  = 5
+        var message = "n = $n"
+        Log.e("kt",message)
+        var b:String? = null
+        var len = b?.length
+        Log.e("kt","b length = $len")
+       // Log.e("kt","b length = $len")
     }
 }
